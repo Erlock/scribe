5cc33ce1aa35ca1304aa0c8fa6d367169b0cd0cd
Sebastian Ion <sebastian.ion100@gmail.com>
2018-12-16 19:42:17 +0000

    Fix note assignment by frequency

--- note.py
+++ note.py
@@ -12,7 +12,7 @@ def closest_note(freq):
 
         index = n % 12
 
-        return NOTES[index], 4 + round(n / 12) + (index > 2)
+        return NOTES[index], 4 + int(n / 12) + (index > 2)
     except ValueError:
         print(f'Frequency {freq} fails')
 
