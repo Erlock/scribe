4e70c9c25bef947e70e36344f76839fc9f641c00
Sebastian Ion <sebastian.ion100@gmail.com>
2018-12-18 15:24:51 +0200

    Solve edge cases for beginning and end of audio file (first and last note might not have enough 0's before/after them)

--- tests/time_domain.py
+++ tests/time_domain.py
@@ -12,6 +12,10 @@ def get_onsets(data, srate, c=2):
     i = 0
     onsets = list()
     while i < len(data):
+        while i < len(data) and data[i] == 0:
+            i += 1
+        if i >= len(data):
+            break
         j = i + 1
         empties = 0
         while j < len(data) and empties <= c:
@@ -20,10 +24,11 @@ def get_onsets(data, srate, c=2):
             else:
                 empties = 0
             j += 1
-        if empties > c and data[i] != 0:
-            onsets.append((i, (j - c + 1)))
-        while j < len(data) and data[j] == 0:
-            j += 1
+        if data[i] != 0:
+            if empties > c:
+                onsets.append((i, (j - c)))
+            elif j >= len(data):
+                onsets.append((i, len(data) - 1))
         i = j
     return onsets
 
@@ -32,16 +37,28 @@ def get_onsets(data, srate, c=2):
 if __name__ == '__main__':
 
     test_dir = 'resources/test_set/'
-    test_filename = 'wrecking_ball.wav'
+    test_filename = 'te_deum.wav'
 
     srate, data = wavutils.read(test_dir + test_filename)
 
-    #plt.plot(data)
+    figure, ax = plt.subplots(2)
+    ax[0].plot(data)
 
     print(f'Starting onset detection for {test_filename}')
     filtered_data = filter_signal(data)
-    print(get_onsets(filtered_data, srate, 200))
-    plt.plot(filtered_data)
+    chunks = get_onsets(filtered_data, srate, 1000)
+
+    indices = np.ones(len(data), dtype=bool)
+
+    for i in range(len(chunks)):
+        for j in range(chunks[i][0], chunks[i][1]):
+            indices[j] = False
+
+    cpdata = data
+    cpdata.setflags(write=True)
+    cpdata[indices] = 0
+
+    ax[1].plot(cpdata)
     plt.show()
     #slen = len(data)
 
