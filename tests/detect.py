a95562db748c6a10db195b1e54c6fd6c907af37d
Sebastian Ion <sebastian.ion100@gmail.com>
2018-12-18 12:41:01 +0200

    Refactor detection into multiple functions

--- tests/detect.py
+++ tests/detect.py
@@ -6,47 +6,55 @@ import wavgen
 import windows
 import time_domain
 
-test_dir = 'resources/test_set/'
-test_filename = 'wrecking_ball.wav'
 
-srate, data = wavutils.read(test_dir + test_filename)
+def detect_notes(data, srate):
+    i = 0
+    spect = list()
+    freqs = list()
 
-print(f'Testing for: {test_filename}')
+    filtered_data = time_domain.filter_signal(data)
+    chunks = time_domain.get_onsets(filtered_data, srate, 80)
 
-slen = len(data)
+    for start,end in chunks:
+        t = data[start:(end + 1)]
+        wlen = end - start + 1
+        f = np.fft.fft(t * windows.han(wlen), wlen)
 
-i = 0
-spect = list()
-freqs = list()
-
-filtered_data = time_domain.filter_signal(data)
-chunks = time_domain.get_onsets(filtered_data, srate, 80)
-
-for start,end in chunks:
-    t = data[start:(end + 1)]
-    wlen = end - start + 1
-    f = np.fft.fft(t * windows.han(wlen), wlen)
-
-    spect.append(np.abs(f[0:round(wlen/2)]))
-    freq_spect = np.fft.fftfreq(wlen, 1.0/srate)
-    freqs.append(freq_spect[0:round(len(freq_spect)/2)])
+        spect.append(np.abs(f[0:round(wlen/2)]))
+        freq_spect = np.fft.fftfreq(wlen, 1.0/srate)
+        freqs.append(freq_spect[0:round(len(freq_spect)/2)])
 
 
-base_freqs = list()
-for i in range(len(spect)):
-    base_freqs.append(freqs[i][np.argmax(spect[i])])
+    base_freqs = list()
+    for i in range(len(spect)):
+        base_freqs.append(freqs[i][np.argmax(spect[i])])
 
-notes = [note.closest_note(i) for i in base_freqs]
+    notes = [note.closest_note(i) for i in base_freqs]
 
-print(notes)
+    return notes, chunks
 
-song = np.array([])
-prev_chunk = (0.0, 0.0)
-for i in range(len(notes)):
-    freq = note.freq_by_note(notes[i][0], notes[i][1])
-    song = np.append(song, np.zeros(round(chunks[i][0] - prev_chunk[1])))
-    song = np.append(song, wavgen.sine_wave(freq, round(chunks[i][1] -
-        chunks[i][0]), srate))
-    prev_chunk = chunks[i]
+def write_song(filename, srate, notes, chunks):
+    song = np.array([])
+    prev_chunk = (0.0, 0.0)
+    for i in range(len(notes)):
+        freq = note.freq_by_note(notes[i][0], notes[i][1])
+        song = np.append(song, np.zeros(round(chunks[i][0] - prev_chunk[1])))
+        song = np.append(song, wavgen.sine_wave(freq, round(chunks[i][1] -
+            chunks[i][0]), srate))
+        prev_chunk = chunks[i]
 
-wavutils.write('result.wav', srate, song)
+    wavutils.write(filename, srate, song)
+
+
+if __name__ == '__main__':
+
+    test_dir = 'resources/test_set/'
+    test_filename = 'wrecking_ball.wav'
+
+    srate, data = wavutils.read(test_dir + test_filename)
+
+    print(f'Testing for: {test_filename}')
+
+    notes, chunks = detect_notes(data, srate)
+
+    write_song(f'result_{test_filename}', srate, notes, chunks)
