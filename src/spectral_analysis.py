6237a6c30d67f2e214b634e91316d3718344e1a6
Sebastian Ion <sebastian.ion100@gmail.com>
2019-04-17 21:12:15 +0100

    Add code to identify base frequency by checking coverage of harmonic spectrum (computes score by using total energy)

--- src/spectral_analysis.py
+++ src/spectral_analysis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import detect
 import scipy.io.wavfile as wavutils
 import windows
+import note
 import sys
 
 
@@ -12,6 +13,54 @@ def get_list_of_note_indices(freq_bin, threshold=2.5e5):
         if freq_bin[i + 1] > threshold:
             indices.append(i + 1)
     return indices
+
+def get_base_frequency(freq_bin, threshold=2.5e5):
+
+    freqs = list()
+    amps = list()
+
+    for freq, amp in freq_bin:
+        if amp > threshold:
+            freqs.append(freq)
+            amps.append(amp)
+
+    notes = [note.closest_note(x) for x in freqs]
+
+    note_bin = list()
+
+    weights = dict()
+
+    for i, n in enumerate(notes):
+        if n not in note_bin:
+            note_bin.append(n)
+            weights[n] = 0.
+        weights[n] += amps[i]
+
+    base = note_bin[0]
+    best_match = 0
+
+    total_energy = np.sum(amps)
+
+    for base_candidate, octave in note_bin:
+        harmonics = note.generate_harmonics(base_candidate, octave)
+
+        harm_match = 0
+
+        for harm in harmonics:
+            if harm in note_bin:
+                harm_match += weights[harm]
+
+        spect_match = harm_match / len(note_bin)
+        harm_match = harm_match / total_energy
+        score = spect_match * harm_match
+
+        if score > best_match:
+            best_match = score
+            base = base_candidate, octave
+
+    index = note_bin.index(base)
+
+    return base, freqs[index], amps[index], score
 
 
 if __name__ == '__main__':
